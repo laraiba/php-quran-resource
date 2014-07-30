020a724388e6972c37b7fb07364ef70be0cf498a
Andy Librian <andylibrian@gmail.com>
2014-07-30 10:51:41 +0700

    added more test cases

--- tests/LaraibaTest/Resource/Surat/SuratRepositoryFactoryTest.php
+++ tests/LaraibaTest/Resource/Surat/SuratRepositoryFactoryTest.php
@@ -10,11 +10,27 @@ class SuratRepositoryFactoryTest extends \PHPUnit_Framework_TestCase
     {
         $suratRepositoryFactory = new SuratRepositoryFactory();
 
+        $initializer1 = $this->getMock('Laraiba\Resource\Surat\SuratInitializerInterface');
+        $suratRepositoryFactory->addInitializer($initializer1);
+
         $suratRepository = $suratRepositoryFactory->createSuratRepository();
 
         $this->assertInstanceOf('Laraiba\Resource\Surat\SuratRepositoryInterface', $suratRepository);
 
         $surat = $suratRepository->findOneBySuratNumber('114');
         $this->assertInstanceOf('Laraiba\Resource\Surat\SuratInterface', $surat);
+    }
+
+    public function testAddInitializers()
+    {
+        $suratRepositoryFactory = new SuratRepositoryFactory();
+
+        $initializer1 = $this->getMock('Laraiba\Resource\Surat\SuratInitializerInterface');
+        $initializer2 = $this->getMock('Laraiba\Resource\Surat\SuratInitializerInterface');
+
+        $suratRepositoryFactory->addInitializer($initializer1);
+        $suratRepositoryFactory->addInitializer($initializer2);
+
+        $this->assertEquals(array($initializer1, $initializer2), $suratRepositoryFactory->getInitializers());
     }
 }

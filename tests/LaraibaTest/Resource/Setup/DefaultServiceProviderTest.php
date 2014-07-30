38fcfab0868fcf9195190b4b0d115c26af030a4d
Andy Librian <andylibrian@gmail.com>
2014-07-30 10:31:48 +0700

    added default service provider test

---
+++ tests/LaraibaTest/Resource/Setup/DefaultServiceProviderTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaraibaTest\Resource\Setup;
+
+use Laraiba\Resource\Setup\DefaultServiceProvider;
+
+class DefaultServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testImplementsServiceProviderInterface()
+    {
+        $d = new DefaultServiceProvider();
+
+        $this->assertInstanceOf('Pimple\ServiceProviderInterface', $d);
+    }
+
+    public function testRegister()
+    {
+        $d = new DefaultServiceProvider();
+        $pimple = new \Pimple\Container();
+
+        $d->register($pimple);
+
+        $this->assertNotNull($pimple['laraiba.ayat_repository']);
+        $this->assertNotNull($pimple['laraiba.ayat_repository_factory']);
+        $this->assertNotNull($pimple['laraiba.surat_repository']);
+        $this->assertNotNull($pimple['laraiba.surat_repository_factory']);
+    }
+}
